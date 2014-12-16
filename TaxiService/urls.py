ccb421d2bb5df0b00b19ad5ae2cd429f72cf2496
hensh <master.hensh@gmail.com>
2014-12-16 16:42:47 +0300

    refactoring

--- TaxiService/urls.py
+++ TaxiService/urls.py
@@ -8,7 +8,7 @@ urlpatterns = patterns('',
     url(r'^accounts/login/$', views.login_user, name='login'),
     url(r'^accounts/logout/$', views.logout_user, name='logout'),
 
-    url(r'^accounts/create/$', admin_views.account_edit, name='create_account'),
+    url(r'^accounts/create/$', admin_views.create, name='create_account'),
     url(r'^accounts/(?P<user_id>\d+)/$', admin_views.account_info, name='account_info'),
     url(r'^accounts/(?P<user_id>\d+)/edit/$', admin_views.account_edit, name='edit_account'),
     url(r'^accounts/$', admin_views.accounts, name='accounts'),
--- TaxiService/views/admin_views.py
+++ TaxiService/views/admin_views.py
@@ -3,27 +3,38 @@ __author__ = 'hensh'
 from TaxiService.required_group_test import group_required
 from TaxiService.user_groups import *
 from django.shortcuts import render_to_response
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect, Http404
 from django.template import RequestContext
+
+def __fill_user_groups(user, request):
+    is_admin = request.POST.get('is_admin', None)
+    is_driver = request.POST.get('is_driver', None)
+    is_dispatcher = request.POST.get('is_dispatcher', None)
+
+    if is_admin:
+        user.groups.add(get_group_admin())
+    if is_driver:
+        user.groups.add(get_group_driver())
+    if is_dispatcher:
+        user.groups.add(get_group_dispatcher())
+    return user
 
 # TODO redirect to error view
 @group_required('Admins')
 def account_edit(request, user_id):
     if request.POST:
+        # TODO try password
         password = request.POST['password']
-        username = request.POST['username']
-        email = request.POST['email']
-        is_admin = request.POST.get('is_admin', None)
-        is_driver = request.POST.get('is_driver', None)
-        is_dispatcher = request.POST.get('is_dispatcher', None)
-        new_user = User.objects.create_user(username, email, password)
-        if is_admin:
-            new_user.groups.add(get_group_admin())
-        if is_driver:
-            new_user.groups.add(get_group_driver())
-        if is_dispatcher:
-            new_user.groups.add(get_group_dispatcher())
-        new_user.save()
+
+        try:
+            user = User.objects.get(id=user_id)
+        except User.DoesNotExist:
+            return Http404
+        user.username = request.POST['username']
+        user.email = request.POST['email']
+
+        user = __fill_user_groups(user, request)
+        user.save()
         return HttpResponseRedirect('/accounts/')
     watching_user = User.objects.get(id=user_id)
     context = RequestContext(
@@ -34,6 +45,26 @@ def account_edit(request, user_id):
     )
     return render_to_response('account/edit.html', context)
 
+@group_required('Admins')
+def create(request):
+    if request.POST:
+        new_user = User.objects.create_user(
+            request.POST['username'],
+            request.POST['email'],
+            request.POST['password']
+        )
+        new_user = __fill_user_groups(new_user, request)
+        new_user.save()
+        context = RequestContext(
+            request,
+            {
+                'watching_user' : new_user,
+            },
+        )
+        return render_to_response('account/info.html', context)
+    return render_to_response('account/create.html', RequestContext(request))
+
+@group_required('Admins')
 def account_info(request, user_id):
     watching_user = User.objects.get(id=user_id)
     context = RequestContext(
@@ -44,7 +75,7 @@ def account_info(request, user_id):
         )
     return render_to_response('account/info.html', context)
 
-# TODO NOT IMPLEMENTED
+@group_required('Admins')
 def accounts(request):
     users = User.objects.all()
     context = RequestContext(
