9777ce6f7be76e7232244420212a27eaea1381d1
hensh <master.hensh@gmail.com>
2014-12-19 16:03:05 +0300

    refactroing

--- TaxiService/views/ride_views.py
+++ TaxiService/views/ride_views.py
@@ -3,13 +3,34 @@ __author__ = 'hensh'
 from django.shortcuts import render_to_response
 from TaxiService.required_group_test import group_required
 from django.template import RequestContext
+from TaxiService.models import Car
+from django.http import HttpResponseRedirect, Http404
+from datetime import datetime
 
 def edit(request, ride_id):
     return render_to_response('ride/edit.html', RequestContext(request))
 
 def create(request, car_id):
-    return render_to_response('ride/create.html', RequestContext(request))
+    context = RequestContext(
+        request,
+        {
+            'datetime' : datetime.now(),
+            'car_id' : car_id
+        },
+    )
+    return render_to_response('ride/create.html', context)
 
 def all_for_car(request, car_id):
-    return render_to_response('ride/rides.html', RequestContext(request))
+    try:
+        car = Car.objects.get(id = car_id)
+    except Car.DoesNotExist:
+        return Http404
+
+    context = RequestContext(
+        request,
+        {
+            'car' : car,
+        }
+    )
+    return render_to_response('ride/rides.html', context)
 
